0e9ef35d43798df4cbad1edcb1cb360067e9810b
marioaje <marioaje@gmail.com>
2026-07-17 20:31:36 -0600

    segunda parte del hotel

--- front/public/js/hotel.js
+++ front/public/js/hotel.js
@@ -1,10 +1,30 @@
 //Declarion de variables y librerias
 
 import { API } from '../js/config.js';
-// import { hotelModels } from '../js/models/hotelModel.js';
+import { hotelModels } from '../js/models/hotelModel.js';
 
 
-consultarHoteles();
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarHoteles();
+
+    document.getElementById('formularioEditar').addEventListener('submit', (evento) => {
+        evento.preventDefault();
+        actualizarHotel();
+    })
+
+
+    document.getElementById('formularioCrear').addEventListener('submit', (evento) => {
+        evento.preventDefault();
+        crearHotel();
+
+    })
+
+
+
+});
+
 
 
 
@@ -49,7 +69,7 @@ function pintarTabla(dataLista) {
 }
 
 window.cargarDatosFormulario = function (id) {
-    alert("cargando datos en el formulario" + id);
+    // alert("cargando datos en el formulario" + id);
 
     //Cargar los datos del hotel en el formulario
     //GET https://paginas-web-cr.com/Api/hotelApi/hotel/hotel.php?id=1 
@@ -77,6 +97,78 @@ function formularioPintar(data) {
 }
 
 
+function crearHotel() {
+    // { "nombre":"Hotel Paradise", "descripcion":"Hotel 5 estrellas", "telefono":"2222-2222", "correo":"hotel@gmail.com", "sitio_web":"https://hotel.com", "usuario":"Mario" } 
+
+    const nombre = document.getElementById('nombreCrear').value;
+    const descripcion = document.getElementById('descripcionCrear').value;
+    const telefono = document.getElementById('telefonoCrear').value;
+    const correo = document.getElementById('correoCrear').value;
+    const sitio_web = document.getElementById('sitio_webCrear').value;
+    const usuario = "Profe Mario";
+
+    const objetoHotel = new hotelModels(0, nombre, descripcion, telefono, correo, sitio_web, usuario);
+
+
+    fetch(API.HOTEL, { method: 'POST', body: JSON.stringify(objetoHotel) })
+        .then(response => response.json())
+        .then(data => finalzarCrear(data.data))
+        .catch(error => console.error(error));
+
+}
+
+
+
+function finalzarCrear(data) {
+    alert("Hotel creado con exito");
+    consultarHoteles();
+    document.getElementById('formularioCrear').reset();
+
+    const modalCrear = bootstrap.Modal.getInstance(document.getElementById('modalCrear'));
+    modalCrear.hide();
+}
+
+
+function actualizarHotel() {
+    const id = document.getElementById('id').value;
+    const nombre = document.getElementById('nombre').value;
+    const descripcion = document.getElementById('descripcion').value;
+    const telefono = document.getElementById('telefono').value;
+    const correo = document.getElementById('correo').value;
+    const sitio_web = document.getElementById('sitio_web').value;
+    const usuario = "Profe Mario";
+
+    // { "id":1, "nombre":"Hotel Paradise CR", "descripcion":"Actualizado", "telefono":"8888-9999", "correo":"nuevo@gmail.com", "sitio_web":"https://hotelcr.com", "usuario":"Mario" } 
+
+    const objetoHotel = new hotelModels(id, nombre, descripcion, telefono, correo, sitio_web, usuario);
+
+
+    fetch(API.HOTEL, { method: 'PUT', body: JSON.stringify(objetoHotel) })
+        .then(response => response.json())
+        .then(data => finalzarEditar(data.data))
+        .catch(error => console.error(error));
+}
+
+
+function finalzarEditar(data) {
+    alert("Hotel editado con exito");
+    consultarHoteles();
+    document.getElementById('formularioEditar').reset();
+
+    const modalHotel = bootstrap.Modal.getInstance(document.getElementById('modalHotel'));
+    modalHotel.hide();
+}
+
+
+function eliminarHotel(id) {
+    fetch(`${API.HOTEL}/${id}`, { method: 'DELETE' })
+        .then(response => response.json())
+        .then(data => {
+            alert("Hotel eliminado con exito");
+            consultarHoteles();
+        })
+        .catch(error => console.error(error));
+}
 // function cargarDatosFormulario(id) {
 //     alert("cargando datos en el formulario" + id);
 
--- front/public/js/models/hotelModel.js
+++ front/public/js/models/hotelModel.js
@@ -1,4 +1,4 @@
-export class Hotel {
+export class hotelModels {
 
     constructor(
         id = 0,
